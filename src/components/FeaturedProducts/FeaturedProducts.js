e45711fdb55cfb5f9d1fc0123cabb14584182d81
Javier <shiordia@gmail.com>
2018-10-07 12:50:21 -0500

    Add lib/api.js

--- src/components/FeaturedProducts/FeaturedProducts.js
+++ src/components/FeaturedProducts/FeaturedProducts.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 
 export default class FeaturedProducts extends Component {
+  state = {
+    loading: true,
+    error: null
+  };
+
+  componentDidMount() {
+    this.loadFeaturedProducts();
+  }
+
+  loadFeaturedProducts = () => {
+    console.log("load featured");
+  };
+
   render() {
     return (
       <div>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,6 +4,6 @@ import LogoImg from "../../images/mf-logo-black.svg";
 
 export default class Logo extends Component {
   render() {
-    return <img src={LogoImg} />;
+    return <img src={LogoImg} alt="logo" />;
   }
 }
